37815e71edcbaa700b7f4dd41bcac2231f48c34c
Florent Richard <florent.richard@supmaritime.fr>
2025-02-27 15:18:05 +0100

    Forgot the loop

--- src/Update.cpp
+++ src/Update.cpp
@@ -9,42 +9,45 @@ Update::~Update()
 {    
 }
 
-static void TaskLaunchNet(SimulationModel* aModel, Network* aNet, OperatingMode::Mode aMode)
+static void TaskNetwork(SimulationModel* aModel, Network* aNet, OperatingMode::Mode aMode)
 {
   eCmdMsg msgType;
   void* dataCmd = NULL;
   Message inMsg(aModel), outMsg(aModel);
 
-  aNet->WaitMessage(inMsg, msgType, &dataCmd);
-  aModel->updateFromNetwork(msgType, dataCmd);	  
+  while(true)
+    {
+      aNet->WaitMessage(inMsg, msgType, &dataCmd);
+      aModel->updateFromNetwork(msgType, dataCmd);	  
 
-  if(OperatingMode::Secondary == aMode)
-    {
-      std::string msgCtrlOv = outMsg.ControlOverride();
-      aNet->SendMessage(msgCtrlOv);
-    }
-  else if(OperatingMode::Multiplayer == aMode)
-    {
-      std::string msgMpFeedBack = outMsg.MpFeedBack();
-      aNet->SendMessage(msgMpFeedBack);
-    }
-  else
-    {
-      if(aModel->getLoopNumber() % 100 == 0)
+      if(OperatingMode::Secondary == aMode)
 	{
-	  std::string msgKeepAliveScn = aModel->getSerialisedScenario();
-	  aNet->SendMessage(msgKeepAliveScn, true);
+	  std::string msgCtrlOv = outMsg.ControlOverride();
+	  aNet->SendMessage(msgCtrlOv);
 	}
-      else if(aModel->getLoopNumber() % 2 == 0)
+      else if(OperatingMode::Multiplayer == aMode)
 	{
-	  std::string msgKeepAlive = outMsg.KeepAlive();
-	  aNet->SendMessage(msgKeepAlive);
+	  std::string msgMpFeedBack = outMsg.MpFeedBack();
+	  aNet->SendMessage(msgMpFeedBack);
 	}
-     /*else
+      else
 	{
-	  std::string msgKeepAliveShort = outMsg.KeepAliveShort();      
-	  aNet->SendMessage(msgKeepAliveShort);
-	}  */  
+	  if(aModel->getLoopNumber() % 100 == 0)
+	    {
+	      std::string msgKeepAliveScn = aModel->getSerialisedScenario();
+	      aNet->SendMessage(msgKeepAliveScn, true);
+	    }
+	  else if(aModel->getLoopNumber() % 2 == 0)
+	    {
+	      std::string msgKeepAlive = outMsg.KeepAlive();
+	      aNet->SendMessage(msgKeepAlive);
+	    }
+	  /*else
+	    {
+	    std::string msgKeepAliveShort = outMsg.KeepAliveShort();      
+	    aNet->SendMessage(msgKeepAliveShort);
+	    }  */  
+	}
     }
 }
 
@@ -54,7 +57,7 @@ static void TaskLaunchNet(SimulationModel* aModel, Network* aNet, OperatingMode:
 void Update::UpdateNetwork(SimulationModel* aModel, Network* aNet, OperatingMode::Mode aMode)
 {
   std::cout << "Creating thread :: Launch Enet Network" << std::endl;
-  std::thread taskNet(TaskLaunchNet, aModel, aNet, aMode);
+  std::thread taskNet(TaskNetwork, aModel, aNet, aMode);
 
   taskNet.detach();
  
